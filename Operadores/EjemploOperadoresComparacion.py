5db9437ee373985109a0374c658db90d3a9b63b8
Ivan <ivantm24@gmail.com>
2019-09-11 19:32:51 -0400

    operadores aritmeticos y comparacion

---
+++ Operadores/EjemploOperadoresComparacion.py
@@ -0,0 +1,27 @@
+
+# Todos estos operadores es para hacer preguntas
+# devuelven True o False solamente
+
+#  determinar si son iguales == se usan dos signos de igual
+print('print(3 == 2)')
+print(3 == 2)
+son_iguales = 3 == 2
+print('print(son_iguales)')
+print(son_iguales)
+print('print(2 == 2)')
+print(2 == 2)
+print('hola2' == 'hols')
+print('hola2' == 'hola2')
+
+# determinar si son diferentes
+print('print(3 != 2)')
+print(3 != 2)  # True
+print('print(3 != 3)')
+print(3 != 3)  # False
+
+print(4>3)
+print(4<3)
+print(4>=3)
+print(4<=3)
+mayor = 3<4
+print(mayor)
---
+++ Operadores/EjemplosOperadoresAritmeticos.py
@@ -0,0 +1,34 @@
+
+# suma +
+resultado = 12 + 3
+print('Resultado suma:')
+print(resultado)
+print(f'Resultado suma:{resultado}')
+
+# resta -
+print('Resultado resta:')
+print(5-7)
+
+# multiplicacion
+print(4*3)
+
+# division
+print(3/4)
+
+# exponenciacion
+print(5**2)
+
+# concatenacion (suma de strings +)
+nombre = "Alexandra"
+apellido = "Rosario"
+nombre_completo =  nombre + " " + apellido # las comillas es para que agregue el espacio
+print(nombre_completo)
+
+# multiplicar string
+print(4 * "repite cuatro")
+
+# modulo %  devuelve el residuo de la division. usualmente se usa para ver si un numero es par
+print(13 % 2)
+
+# division entera //
+print(10//3)
