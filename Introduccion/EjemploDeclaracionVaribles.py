2e08a5642ab030a6bb72be6fd410e1a57f8978e5
Ivan <ivantm24@gmail.com>
2019-09-11 18:51:30 -0400

    agrega ejemplo declaraciones

---
+++ Introduccion/EjemploDeclaracionVaribles.py
@@ -0,0 +1,48 @@
+# Todo lo que se encuentra despues de un signo de # es un comentario
+# Los comentarios son ignorados por el interprete
+# declarar variable entera
+cantidad_estudiantes = 14
+
+# declarar variable decimal
+precio = 102.98
+
+# declarar variable compleja
+corriente = 3 + 3j
+
+# variable string
+nombre = "Stanley" # linea 12 y 13 son equivalentes
+apellido = 'Rosario'
+parrafo = """Este es un parrafo. Muy largo
+puedo seguir escribiendo abajo."""
+
+# variables booleanas
+tiene_hambre = True
+tiene_sueno = False
+
+# Listas
+estudiantes = ['Stanley', 'Lia', 'Alex']
+numeros_loteria = [12, 214, 543, 767, 4747, 76]
+
+# Tuplas
+dias_semana = ('lunes', 'martes', 'miercoles', 'jueves', 'viernes')
+
+# Diccionarios
+diccionario_estudiante = {
+    '2051632': 'Alexandra',
+    '20160831': 'Maxy',
+    '20174325': 'Daniel'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
