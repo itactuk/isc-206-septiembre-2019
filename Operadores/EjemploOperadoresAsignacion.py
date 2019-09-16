602d5466836b0f9c5e8d27109c76cfb7897e3276
Ivan <ivantm24@gmail.com>
2019-09-16 18:25:36 -0400

    ejemplo operadores de asignacion

---
+++ Operadores/EjemploOperadoresAsignacion.py
@@ -0,0 +1,19 @@
+
+
+precio = 2  # asigna 2 a la variable
+print(precio)
+
+precio += 3  # es equivalente a hacer precio = precio + 3
+print(precio)
+
+precio-=4
+print(precio)
+
+precio*=2
+print(precio)
+
+precio**=2
+print(precio)
+
+precio%=2
+print(precio)
